5553a644fbb408796f4c57956d14e5febcef5dc6
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-03-19 16:29:17 +0100

    Updated and added images

--- src/main/java/io/wollinger/hkmanager/ImageManager.java
+++ src/main/java/io/wollinger/hkmanager/ImageManager.java
@@ -7,13 +7,27 @@ import java.io.IOException;
 public class ImageManager {
     private static BufferedImage geo;
     private static BufferedImage mask;
-    private static BufferedImage soulVessel;
+    private static BufferedImage mask_steel;
+    private static BufferedImage vessel;
+    private static BufferedImage vessel_steel;
+
+    private static BufferedImage hud;
+    private static BufferedImage hud_god;
+    private static BufferedImage hud_steel;
+
     private static BufferedImage[] nails = new BufferedImage[4];
 
     public static void load() {
         geo = load("geo");
         mask = load("mask");
-        soulVessel = load("soulvessel");
+        mask_steel = load("mask_steel");
+        vessel = load("vessel");
+        vessel_steel = load("vessel_steel");
+
+        hud = load("hud");
+        hud_god = load("hud_god");
+        hud_steel = load("hud_steel");
+
         for(int i = 0; i < nails.length; i++)
             nails[i] = load("nail" + (i + 1));
     }
@@ -35,8 +49,8 @@ public class ImageManager {
         return mask;
     }
 
-    public BufferedImage getSoulVessel() {
-        return soulVessel;
+    public BufferedImage getVessel() {
+        return vessel;
     }
 
     public BufferedImage getNail(int index) {
