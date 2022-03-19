06a47b515f803f288d9fa3ad1ff4a82a57c61303
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-03-19 14:48:46 +0100

    Added savegame loading

--- src/main/java/io/wollinger/hkmanager/Save.java
+++ src/main/java/io/wollinger/hkmanager/Save.java
@@ -1,11 +1,23 @@
 package io.wollinger.hkmanager;
 
+import org.json.JSONObject;
+
 import java.io.File;
 import java.util.ArrayList;
 
 public class Save {
     private ArrayList<File> files = new ArrayList<>();
     private boolean valid = false;
+
+    private String name;
+
+    private int geo;
+    private float completionPercentage;
+    private float playTime;
+    private int nailUpgrades;
+    private String version;
+    private int masks;
+    private boolean steelsoul;
 
     public enum KIND {USER1, USER2, USER3, USER4}
 
@@ -21,9 +33,30 @@ public class Save {
             if(cFile.getName().equals(lookingFor + ".dat"))
                 loadData(cFile);
         }
+
+        File infoFile = new File(hkFolder.getAbsolutePath() + File.separator + lookingFor + ".json");
+        if(infoFile.exists())
+            loadInfo(infoFile);
     }
 
     private void loadData(File file) {
+        try {
+            JSONObject saveData = SaveLoader.loadSave(file);
+            JSONObject playerData = saveData.getJSONObject("playerData");
+
+            geo = playerData.getInt("geo");
+            completionPercentage = playerData.getFloat("completionPercentage");
+            playTime = playerData.getFloat("playTime");
+            nailUpgrades = playerData.getInt("nailSmithUpgrades");
+            version = playerData.getString("version");
+            masks = playerData.getInt("maxHealth");
+            steelsoul = playerData.getBoolean("permadeathMode");
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void loadInfo(File file) {
 
     }
 
