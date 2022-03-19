e19de6b9cb9c83910135884e16b541ad2e0b7376
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-03-19 13:12:03 +0100

    Create boilerplate folder code

---
+++ src/main/java/io/wollinger/hkmanager/HKManager.java
@@ -0,0 +1,38 @@
+package io.wollinger.hkmanager;
+
+import javax.swing.*;
+import java.io.File;
+import java.util.ArrayList;
+
+public class HKManager extends JFrame {
+    private ArrayList<Save> saves = new ArrayList<>();
+
+    public HKManager() {
+        setSize(512, 512);
+        setTitle("HKManager");
+        ensureFolders();
+    }
+
+    public void ensureFolders() {
+        String userHome = System.getProperty("user.home");
+        String mainFolderPath = userHome + File.separator + ".hkmanager";
+
+        File mainFolder = new File(mainFolderPath);
+        File savesFolder = new File(mainFolderPath + File.separator + "saves");
+        File backupFolder = new File(mainFolder + File.separator + "backups");
+        File logFolder = new File(mainFolder + File.separator + "logs");
+
+        //TODO: When add logger, add checks
+        if(!mainFolder.exists())
+            mainFolder.mkdir();
+
+        if(!savesFolder.exists())
+            savesFolder.mkdir();
+
+        if(!backupFolder.exists())
+            backupFolder.mkdir();
+
+        if(!logFolder.exists())
+            logFolder.mkdir();
+    }
+}
