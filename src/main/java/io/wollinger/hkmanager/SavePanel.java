642cefec43d08f55b45c761fa9fb5ad27950f4a2
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-03-19 16:36:38 +0100

    Added base resize and render code to SavePanel

--- src/main/java/io/wollinger/hkmanager/SavePanel.java
+++ src/main/java/io/wollinger/hkmanager/SavePanel.java
@@ -1,7 +1,22 @@
 package io.wollinger.hkmanager;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class SavePanel extends JPanel {
+    public SavePanel(Save save, JPanel parent) {
+        SavePanelRenderer renderer = new SavePanelRenderer(save);
 
+        parent.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                renderer.setPreferredSize(new Dimension(parent.getWidth(), parent.getWidth() / 2));
+                revalidate();
+            }
+        });
+
+        add(renderer);
+    }
 }
