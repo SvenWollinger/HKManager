85e5fb83a74bfa98ab7ecb1bdbfcc1940b8fbdc8
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-03-19 17:58:17 +0100

    Playing with rendering

--- src/main/java/io/wollinger/hkmanager/SavePanelRenderer.java
+++ src/main/java/io/wollinger/hkmanager/SavePanelRenderer.java
@@ -14,24 +14,47 @@ public class SavePanelRenderer extends JPanel {
 
     @Override
     public void paint(Graphics g) {
-        boolean ss = save.isSteelsoul();
+        int mode = save.getMode();
 
-        int maskSize = getWidth() / 9;
-        BufferedImage mask = ss ? ImageManager.mask_steel : ImageManager.mask;
+        BufferedImage hud = ImageManager.hud;
+        Point point = ImageManager.hud_point;
+        float scaling = ImageManager.hud_scale;
+        switch(mode) {
+            case 0:
+                hud = ImageManager.hud_steel;
+                point = ImageManager.hud_steel_point;
+                scaling = ImageManager.hud_steel_scale;
+                break;
+            case 2:
+                hud = ImageManager.hud_god;
+                point = ImageManager.hud_god_point;
+                scaling = ImageManager.hud_god_scale;
+                break;
+        }
+
+        Dimension hudDim = getScaledDimension(hud, new Dimension((int)(getWidth() / scaling), (int)(getHeight() / scaling)));
+        g.drawImage(hud, 0, 0, hudDim.width, hudDim.height, this);
+        int offsetX = (int) ((hudDim.width * (point.x / 100F)));
+        int offsetY = (int) ((hudDim.height * (point.y / 100F)));
+
+        int maskSize = (getWidth() - offsetX) / 9;
+        BufferedImage mask = mode == 1 ? ImageManager.mask_steel : ImageManager.mask;
         for(int i = 0; i < save.getMasks(); i++) {
             //We multiply by 1000 so that the dimension grows if possible
-            Dimension dim = getScaledDimension(new Dimension(mask.getWidth()*1000, mask.getHeight()*1000), new Dimension(maskSize, maskSize));
-            g.drawImage(mask, maskSize * i, 0, dim.width, dim.height, this);
+            Dimension dim = getScaledDimension(mask, new Dimension(maskSize, maskSize));
+            g.drawImage(mask, offsetX + maskSize * i, offsetY, dim.width, dim.height, this);
         }
 
         BufferedImage nail = ImageManager.nails[save.getNailUpgrades() - 1];
-        Dimension nailDim = getScaledDimension(new Dimension(nail.getWidth()*1000, nail.getHeight()*1000), new Dimension(maskSize, maskSize * 2));
-        g.drawImage(nail, 0, maskSize, nailDim.width, nailDim.height, this);
+        Dimension nailDim = getScaledDimension(nail, new Dimension(maskSize, maskSize * 2));
+        g.drawImage(nail, offsetX, maskSize + offsetY, nailDim.width, nailDim.height, this);
+
+        g.drawRect(0, 0, getWidth()-1, getHeight()-1);
     }
 
-    public static Dimension getScaledDimension(Dimension imgSize, Dimension boundary) {
-        int original_width = imgSize.width;
-        int original_height = imgSize.height;
+    public static Dimension getScaledDimension(BufferedImage img, Dimension boundary) {
+        int original_width = img.getWidth() * 9999;
+        int original_height = img.getHeight() * 9999;
         int bound_width = boundary.width;
         int bound_height = boundary.height;
         int new_width = original_width;
