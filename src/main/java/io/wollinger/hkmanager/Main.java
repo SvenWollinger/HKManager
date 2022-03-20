2940e6c057eee0a61486727695e896a7d4d79b90
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-03-20 08:15:21 +0100

    Added theme to UIManager

--- src/main/java/io/wollinger/hkmanager/Main.java
+++ src/main/java/io/wollinger/hkmanager/Main.java
@@ -1,7 +1,22 @@
 package io.wollinger.hkmanager;
 
+import com.formdev.flatlaf.FlatIntelliJLaf;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatIntelliJLaf());
+            UIManager.put("ScrollBar.showButtons", true);
+            UIManager.put("ScrollBar.width", 16 );
+            UIManager.put("TabbedPane.showTabSeparators", true);
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            JDialog.setDefaultLookAndFeelDecorated(true);
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
         ImageManager.load();
         new HKManager();
     }
